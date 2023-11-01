d818ab56f525e06019a0fb25e6f7c1de1ff00119
sravsveeranki <veerankisaisravaniusa@gmail.com>
2023-11-01 01:38:26 -0500

    modified app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var indexRouter = require('./routes/KitchenItems');
+var KitchenItemsRouter = require('./routes/KitchenItems');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/users', KitchenItemsRouter);
+app.use('/KitchenItems', KitchenItemsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
